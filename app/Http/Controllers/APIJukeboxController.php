81db7682980dca0d793a3639a12931608cab4a46
haianhxanh <hanka7n@gmail.com>
2020-10-22 09:30:04 +0200

    testing

---
+++ app/Http/Controllers/APIJukeboxController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APIJukeboxController extends Controller
+{
+    public function index()
+    {
+        return "HELLOOO";
+    }
+}
